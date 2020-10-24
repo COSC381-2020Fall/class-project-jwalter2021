53b5de89e817cd70bdd135c15213521cdc7a5700
EC2 Default User <ec2-user@ip-172-26-9-86.ec2.internal>
2020-10-24 23:52:21 +0000

    Added paging to Whoosh query script and modified create_data_for_indexing for functionality.

--- create_data_for_indexing.py
+++ create_data_for_indexing.py
@@ -5,15 +5,16 @@ paths = [str(x) for x in Path('./youtube_data').glob('**/*.json')]
 results = []
 for path in paths:
     with open(path, 'r') as f:
-        data = {}
         data = json.load(f)
-        # insert your code here
-        for p in data['videoinfo']:
-            data['videoinfo'].append('id: ' + p['id'])
-            data['videoinfo'].append('title: ' + p['title'])
-            data['videoinfo'].append('description: ' + p['description'])
-            data['videoinfo'].append('')
-            results.append(data)
-
+        video = data['items'][0]
+        video_id = video['id']
+        title = video['snippet']['title']
+        description = video['snippet']['description']
+        results.append({
+            'id': video_id,
+            'title': title,
+            'description': description
+            })
+    
 with open('data_for_indexing.json', 'w') as dump_file:
     json.dump(results, dump_file)
