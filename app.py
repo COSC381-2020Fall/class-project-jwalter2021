72de5f9b295adc142a56c184ab5ce1cfaef2486b
EC2 Default User <ec2-user@ip-172-26-12-147.ec2.internal>
2020-12-13 23:09:01 +0000

    Final Project

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def handle_query():
 
 @app.route("/query_view", strict_slashes=False)
 def handle_query_view():
+
     query_term = request.args.get("q")
     if not query_term:
         query_term = ""
@@ -31,20 +32,25 @@ def handle_query_view():
     if not page_index_arg:
         page_index_arg = "1"
 
-    conn = sqlite3.connect('history.db')
-    c = conn.cursor()
-    c.execute("INSERT INTO search_terms (term, search_time) VALUES (?, strftime('%s', 'now'));", (query_term,))
-    c.execute("SELECT * FROM search_terms;")
-    rows = c.fetchall()
-    conn.commit()
-    conn.close()
+    if query_term != "":
+        conn = sqlite3.connect('history.db')
+        c = conn.cursor()
+        c.execute("SELECT status FROM settings WHERE id=(SELECT MAX(ID) FROM settings);")
+        checkbox = c.fetchone()
+        if checkbox[0] == "enabled":
+            c.execute("INSERT INTO search_terms (topic, term, search_time) VALUES ('Montessori', ?, datetime(strftime('%s','now'),'unixepoch'));", (query_term,))
+        conn.commit()
+        conn.close()
 
-    page_index = int(page_index_arg)
-    query_results = query_on_whoosh.query(query_term, current_page=page_index)
-    search_results = query_results[0]
-    results_cnt = int(query_results[1])
-    page_cnt = math.ceil(results_cnt / 10)
-    return render_template("query.html", results = search_results, page_cnt=page_cnt, query_term=query_term, history_list=rows)
+        page_index = int(page_index_arg)
+        query_results = query_on_whoosh.query(query_term, current_page=page_index)
+        search_results = query_results[0]
+        results_cnt = int(query_results[1])
+        page_cnt = math.ceil(results_cnt / 10)
+        return render_template("query.html", results = search_results, page_cnt=page_cnt, query_term=query_term)
+
+    else: 
+        return render_template("query.html", page_cnt=0)
 
 @app.route("/about", strict_slashes=False)
 def handle_about():
@@ -59,3 +65,41 @@ def handle_request():
     message = "Subject: {}\n\n{}".format("Request to add new data", "request to add: " + new_data)
     server.sendmail("j.rogalski3260@gmail.com", "j.rogalski3260@gmail.com", message)
     return render_template("success.html", new_data=new_data)
+
+@app.route("/history", strict_slashes=False)
+def handle_history():
+    conn = sqlite3.connect('history.db')
+    c = conn.cursor()
+    c.execute("SELECT id, topic, term, search_time FROM search_terms ORDER BY search_time DESC;")
+    rows = c.fetchall()
+    conn.close()
+    return render_template("history.html", history_list=rows)
+
+@app.route("/remove_history", strict_slashes=False)
+def handle_deletion():
+    delRec = request.args.get("q")
+    if not delRec:
+        delRec = "1"
+    conn = sqlite3.connect('history.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM search_terms WHERE id = ?;", (delRec,))
+    conn.commit()
+    conn.close()
+    return render_template("remove_history.html", delRec=delRec)
+
+@app.route("/settings", strict_slashes=False)
+def settings():
+    return render_template("settings.html")
+
+@app.route("/save_settings", strict_slashes=False)
+def save_settings():
+    checkbox = request.args.get('history')
+    if checkbox == "on":
+        checkbox = "enabled"
+    else: checkbox = "disabled"
+    conn = sqlite3.connect('history.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO settings (status) VALUES (?);", (checkbox,))
+    conn.commit()
+    conn.close()
+    return render_template("save_settings.html", value=checkbox)
